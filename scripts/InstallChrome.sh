b00627d1b82626dd60f80a1ff3bf001849769d1e
moatazeldebsy <moataznabil2019@gmail.com>
2020-03-04 12:25:50 +0100

    change script

--- scripts/InstallChrome.sh
+++ scripts/InstallChrome.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
-wget -q https://script.install.devinsideyou.com/google-chrome
-sudo chmod +x google-chrome && ./google-chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
